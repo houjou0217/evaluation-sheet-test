43b78de5529aea495a5daa2614bde5200ae8f49f
Shinnosuke.Hojo <shinnosuke.houjou0217@gmail.com>
2025-02-27 11:21:03 +0900

    2025/2/27

---
+++ wordpress/html/wp-content/cocoon-child-master/evaluation-sheet/sql/jp_answer_count.sql
@@ -0,0 +1,4 @@
+CREATE TABLE jp_answer_count (
+    wp_user_id INT NOT NULL,
+    is_completed BOOLEAN NOT NULL
+);
---
+++ wordpress/html/wp-content/cocoon-child-master/evaluation-sheet/sql/jp_field_average.sql
@@ -0,0 +1,4 @@
+CREATE TABLE jp_field_average (
+    wp_user_id INT NOT NULL,
+    average_score FLOAT NOT NULL
+);
